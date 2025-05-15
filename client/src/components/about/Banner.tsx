d0b64c090e8b2c501e065ab792f4331fd575d3d0
kazimehedihasan1 <mehedihasanarabi00@gmail.com>
2025-05-15 17:10:50 +0600

    add button in about page

--- client/src/components/about/Banner.tsx
+++ client/src/components/about/Banner.tsx
@@ -1,15 +1,14 @@
 import { Button } from "../ui/button";
-import Icon from "@/components/about/BgIcon";
 const Banner = () => {
   return (
     <div>
-      <div className="bg-white p-14 rounded-xl flex items-center justify-between  gap-4">
+      <div className="bg-white p-14 rounded-xl flex flex-col sm:flex-row sm:items-center justify-between  gap-4">
         <div className="space-y-2">
           <h3 className="font-semibold text-gray-900">
-            <span className="text-[var(--color-bgPrimary)] text-[15px] sm:text-[25px] font-semibold leading-[100%] inline-block">
+            <span className="text-[var(--color-bgPrimary)] text-[18px] sm:text-[25px] font-semibold leading-[100%] inline-block">
               Together
             </span>
-            <span className="text-[15px] sm:text-[25px]">
+            <span className="text-[18px] sm:text-[25px]">
               , let's shape the future of digital
               <br /> innovation
             </span>
@@ -19,9 +18,7 @@ const Banner = () => {
             in design and development.
           </p>
         </div>
-        <div>
-            <img src={Icon} alt="" />
-        </div>
+        
         <Button
           size="lg"
           className="bg-[var(--color-bgPrimary)] rounded-[8px] cursor-pointer hover:bg-[var(--color-bgPrimary)]"
