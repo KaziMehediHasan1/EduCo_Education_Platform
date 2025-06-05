dd26d232d964c7f5c1441bd60b71ee8891898045
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-06-06 00:10:47 +0600

    add user model is successfully done

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 import config from "./config";
-import express from "express";
-const app = express();
+import app from "./app";
 
 const dbConnect = async () => {
   await mongoose.connect(config.dbUrl as string);
-  await app.listen(() => {
-    console.log(`${config.port},DB IS CONNECTED SUCCESSFULLY`);
+  app.listen(config.port, () => {
+    console.log(`http://localhost:${config.port} DB IS CONNECTED SUCCESSFULLY`);
   });
 };
 dbConnect();
